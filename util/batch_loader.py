882cfa3cb61293e5a7e251b6388e11a267ddb0b6
LittleLittleCloud <2260578356@qq.com>
2018-03-21 22:08:21 +0800

    fix small bug in batch_loader

--- util/batch_loader.py
+++ util/batch_loader.py
@@ -55,12 +55,12 @@ class Batch:
         data=self.data
         np.random.shuffle(self.test_index)
         cur=0
-        while cur<len(self.train_index):
+        while cur<len(self.test_index):
             start=cur
-            end=min(cur+batch_size,len(self.train_index))
+            end=min(cur+batch_size,len(self.test_index))
             cur=end
-            encode_input=[data[i] for i in self.train_index[start:end]]
-            decode_input=[[0] for i in self.train_index[start:end]]
+            encode_input=[data[i] for i in self.test_index[start:end]]
+            decode_input=[[0] for i in self.test_index[start:end]]
             max_len=max(len(x) for x in encode_input)
 
             #sorry again
@@ -68,13 +68,9 @@ class Batch:
                 to_add=max_len-len(line)
                 encode_input[i]=[2]*to_add+line[::-1]
             encode_input=np.array(encode_input)
-
-            for i,line in enumerate(decode_input):
-                to_add=max_len+1-len(line)
-                decode_input[i]=line+[2]*to_add
             decode_input=np.array(decode_input)
 
-            assert decode_input.shape==(encode_input.shape[0],encode_input.shape[1]+1)
+            assert decode_input.shape==(encode_input.shape[0],1)
             yield encode_input,decode_input
 
 
